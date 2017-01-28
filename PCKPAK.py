3be573cb4c908ee30e6022500dac7f7d36f8bd50
Tim Keller <geekboy1011@gmail.com>
2017-01-27 19:17:48 -0500

    Initial commit

---
+++ PCKPAK.py
@@ -0,0 +1,81 @@
+import argparse
+import os
+from struct import *
+
+parser = argparse.ArgumentParser(description='Generate Pck archive')
+parser.add_argument("Input",help='Input Folder')
+parser.add_argument("Output",help='Output File')
+args = parser.parse_args()
+'''
+Header
+File Table
+FileData
+ 
+Header            | Total Size: 0x80
+------------------------------------
+FileCount         | Size: 0x4
+Archive Size      | Size: 0x4
+Padding           | Size: 0x78
+ 
+ 
+File Table  Total | Size: NumFiles*0x80
+---------------------------------------
+File Name         | Size: 0x16
+???               | Size: 0x2         ;Depacker treats this as part of the file name?  Changes on each file
+Offset To File    | Size: 0x4         ;From Archive Start
+Size in Archive   | Size: 0x4
+Uncompressed Size | Size: 0x4         ;If file is a compressed stream these will differ. 
+Padding           | Size 0x5C
+ 
+FileData          | Total Size:  ArchiveSize-FileTable-Header
+Appended to the table is the stream of all assets marked out by earlier files.
+'''
+
+FileMetaData = []
+
+for i in os.listdir(args.Input):
+    # with open('{}/{}'.format(args.Input,i)) as f:
+    name = i.split('-')[1]
+    rID = i.split('-')[0]
+    size = os.stat('{}/{}'.format(args.Input,i)).st_size
+    FileMetaData.append([name,size,0,rID])
+    print '{}|{}|{}|{}'.format(name,size,0,rID)
+# Name | Size | Offset | Resource ID
+
+NumFiles = len(FileMetaData)
+OFS = 0x80 + (NumFiles*0x80)
+
+previousFileSize = 0
+for i in range(len(FileMetaData)):
+    FileMetaData[i][2]=OFS+previousFileSize
+    OFS = FileMetaData[i][2]
+    previousFileSize = FileMetaData[i][1]
+    FullSize = FileMetaData[i][2]+previousFileSize
+
+
+
+with open(args.Output,'w+b') as f:
+    f.write(pack('L',NumFiles))
+    f.write(pack('L',FullSize))
+#    array('L',NumFiles).tofile(f)
+#    array('L',FullSize).toFile(f)
+    f.seek(0x78,1)
+    for i in range(len(FileMetaData)):
+        saveOFS = f.tell()
+        f.write(FileMetaData[i][0])
+        f.seek(saveOFS)
+        f.seek(0x16,1)
+        f.write(pack('H',int(FileMetaData[i][3])))
+        f.write(pack('L',FileMetaData[i][2]))
+        f.write(pack('L',FileMetaData[i][1]))
+        f.write(pack('L',FileMetaData[i][1]))
+        f.seek(0x5C,1)
+
+
+    for i in range(len(FileMetaData)):
+        with open('{}/{}-{}'.format(args.Input,FileMetaData[i][3],FileMetaData[i][0]),'rb') as t:
+                f.write(t.read())
+
+
+
+
---
+++ README.md
@@ -0,0 +1,31 @@
+Some files I have been working on for translating Final Fantasy 1 for the 3ds
+
+#File List:
+##PCK file Spec.txt
+Goes over file spec for pck/dpk files
+
+##PCKPAK.py
+PCK packer, Currently does not making working PCK's crashes my 3ds when
+build it as a cia or use NTR/LayeredFS. With Hans..it just does somethign
+funky game doesnt crash but fails to load assets?
+
+##PCKUNPACK.py
+Unpacks PCK files, Requires WP16.exe to decompress files
+
+##wp16.c WP16.exe
+decompressor used by this version of ff1 as well as the psp version, Code was
+found on a forum somewhere back when this started. Credits to the author for
+the algorithm.
+
+
+#USE
+
+pckunpack.py FILETOUNPACK "FOLDER\TO\UNPACK\TO"
+
+Decompression has been tested most visual assets are compressed and have been
+succsessfully been able to extract images. 
+
+Packing of DPK/PCK files still needs work. There is a WORD missing from the
+PCK file structure that I do not know what is nor have I put the time in with
+a debugger to figure out. 
+
